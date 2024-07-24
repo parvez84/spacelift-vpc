93e845178a141f266a252f67c1e19db5ab8c59af
parvez84 <32779753+parvez84@users.noreply.github.com>
2024-07-24 13:46:30 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "subnet_main1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
